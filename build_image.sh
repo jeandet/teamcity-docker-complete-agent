a6ea27841016a0c55ef9fd1095e439377c946f0c
Alexis Jeandet <alexis.jeandet@member.fsf.org>
2018-01-27 20:35:49 +0100

    Fixed small typo
    Signed-off-by: Alexis Jeandet <alexis.jeandet@member.fsf.org>

--- build_image.sh
+++ build_image.sh
@@ -1,5 +1,5 @@
 docker build -t jeandet/teamcity-docker-complete-agent .
-docker run -d=true -e SERVER_URL=https://hephaistos.lpp.polytechnique.fr/teamcity --name=teamcity-docker-complete-agent -it jeandet/teamcity-docker-complete-agent0 &
+docker run -d=true -e SERVER_URL=https://hephaistos.lpp.polytechnique.fr/teamcity --name=teamcity-docker-complete-agent -it jeandet/teamcity-docker-complete-agent &
 sleep 300
 docker stop teamcity-docker-complete-agent
 docker commit teamcity-docker-complete-agent jeandet/teamcity-docker-complete-agent
