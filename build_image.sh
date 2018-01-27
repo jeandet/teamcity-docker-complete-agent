d749ad1a5c6bf9595d2eb2f2b07d80623e6e51df
Alexis Jeandet <alexis.jeandet@member.fsf.org>
2018-01-27 19:15:58 +0100

    Added agent update on image build
    Signed-off-by: Alexis Jeandet <alexis.jeandet@member.fsf.org>

--- build_image.sh
+++ build_image.sh
@@ -1,1 +1,6 @@
 docker build -t jeandet/teamcity-docker-complete-agent .
+docker run -d=true -e SERVER_URL=https://hephaistos.lpp.polytechnique.fr/teamcity --name=teamcity-docker-complete-agent -it jeandet/teamcity-docker-complete-agent0 &
+sleep 300
+docker stop teamcity-docker-complete-agent
+docker commit teamcity-docker-complete-agent jeandet/teamcity-docker-complete-agent
+docker rm teamcity-docker-complete-agent
